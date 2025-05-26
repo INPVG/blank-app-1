22a00ea0037db7ead1f841178fcf6a27a2f50349
INPVG <shienzydc@gmail.com>
2025-05-27 04:11:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("460e998176c1e048838340c809048c6a_8948282656653229215.jpg", width=200)
